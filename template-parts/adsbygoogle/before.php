e1b2af5a022a165bb6147779da9ca5304a9c621f
Marcin Pietrzak <marcin@iworks.pl>
2022-01-31 20:18:30 +0100

    mobile

--- template-parts/adsbygoogle/before.php
+++ template-parts/adsbygoogle/before.php
@@ -1,8 +1,8 @@
 <!-- <?php echo basename( __FILE__ ); ?> -->
-<div class="banners "><script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<div class="banners banners-before"><script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <!-- reklama niestandardowa -->
 <ins class="adsbygoogle"
-	 style="display:inline-block;width:728px;height:160px"
+	 style="height:160px"
 	 data-ad-client="ca-pub-4538250495443917"
 	 data-ad-slot="5952027390"></ins>
 <script>
