f3a4c7318a2c9c26f4b9767368ebd57c0d7b5460
Marcin Pietrzak <marcin@iworks.pl>
2022-01-31 08:26:02 +0100

    working on

--- footer.php
+++ footer.php
@@ -13,17 +13,11 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'instsani-pl' ) ); ?>">
 				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'instsani-pl' ), 'WordPress' );
+				/* translators: %s: year */
+				printf( esc_html__( '&copy; %s Alfred Adamczewski', 'instsani-pl' ), date( 'Y' ) );
 				?>
 			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'instsani-pl' ), 'instsani-pl', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
