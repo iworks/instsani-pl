6a3dcd5d0693e58a602c17361c4fe7e6ade8bc00
Marcin Pietrzak <marcin@iworks.pl>
2022-01-31 12:42:02 +0100

    home on breadcrumbs

--- page.php
+++ page.php
@@ -17,13 +17,15 @@ get_header();
 
 	<main id="primary" class="site-main">
 		<div class="entry-wrapper">
+		<?php if ( ! is_front_page() && ! is_home() ) { ?>
 <div class="breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/">
-	<?php
-	if ( function_exists( 'bcn_display' ) ) {
-		bcn_display();
-	}
-	?>
+			<?php
+			if ( function_exists( 'bcn_display' ) ) {
+				bcn_display();
+			}
+			?>
 </div>
+		<?php } ?>
 		<?php
 		while ( have_posts() ) :
 			the_post();
