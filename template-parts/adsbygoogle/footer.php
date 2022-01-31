599510d92d77a4b8e1f2a67654eb7b25105abf5b
Marcin Pietrzak <marcin@iworks.pl>
2022-01-31 11:20:53 +0100

    ads

--- template-parts/adsbygoogle/footer.php
+++ template-parts/adsbygoogle/footer.php
@@ -1,14 +1,3 @@
-<script type="text/javascript"><!--//--><![CDATA[//><!--
-google_ad_client = "ca-pub-4538250495443917";
-/* wieżowiec szeroki */
-google_ad_slot = "2782128992";
-google_ad_width = 160;
-google_ad_height = 600;
-//--><!]]></script>
-<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-FQ56YPX7ZP"></script>
 <script>
@@ -18,4 +7,8 @@ google_ad_height = 600;
 
   gtag('config', 'G-FQ56YPX7ZP');
 </script>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
 
--- template-parts/adsbygoogle/side.php
+++ template-parts/adsbygoogle/side.php
@@ -1,12 +1,11 @@
 <!-- <?php echo basename( __FILE__ ); ?> -->
-<div>
-<ins
-	class="adsbygoogle"
-	style="display:inline-block;width:160px;height:600px"
-	data-ad-client="ca-pub-4538250495443917"
-	data-ad-slot="2782128992"
-	data-ad-width="160"
-	data-add-height="600"
-></ins>
-</div>
+<div class="banners "><script type="text/javascript"><!--//--><![CDATA[//><!--
+google_ad_client = "ca-pub-4538250495443917";
+/* wieżowiec szeroki */
+google_ad_slot = "2782128992";
+google_ad_width = 160;
+google_ad_height = 600;
+//--><!]]></script><script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"><!--//--><![CDATA[//><!--
+
+//--><!]]></script></div>
 
